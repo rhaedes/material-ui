83fb44dabebf1dd89451ad888dbff20a8a112276
dk\anb <anb@sitecore.net>
2016-05-23 15:55:00 +0200

    #added chart to LandingPages

--- exg/src/app/components/pages/LandingPages.js
+++ exg/src/app/components/pages/LandingPages.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
+import NVD3Chart from 'react-nvd3'
 
 class Page extends Component {
     render() {
+        
+        var datum = [{
+          key: "Cumulative Return",
+          values: [
+            {
+              "label" : "A" ,
+              "value" : -29.765957771107
+            } ,
+            {
+              "label" : "B" ,
+              "value" : 0
+            } ,
+            {
+              "label" : "C" ,
+              "value" : 32.807804682612
+            } ,
+            {
+              "label" : "D" ,
+              "value" : 196.45946739256
+            } ,
+            {
+              "label" : "E" ,
+              "value" : 0.19434030906893
+            } ,
+            {
+              "label" : "F" ,
+              "value" : -98.079782601442
+            } ,
+            {
+              "label" : "G" ,
+              "value" : -13.925743130903
+            } ,
+            {
+              "label" : "H" ,
+              "value" : -5.1387322875705
+            }
+          ]
+        }
+      ];
+      
         return (
-            <div>{window.location.hash}</div>
+            <NVD3Chart id="barChart" type="discreteBarChart" datum={datum} x="label" y="value"/>
         );
     }
 }
