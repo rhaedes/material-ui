efe2a3475c53353fb5ddd3fec8d43bfeeebf82f4
ua\egr <egr@sitecore.net>
2016-05-27 13:40:13 +0300

    Overview tab the rest of the content

--- exg/src/app/components/pages/Overview.js
+++ exg/src/app/components/pages/Overview.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Panel from './internal/Panel.js';
 import PanelRow from './internal/PanelRow.js';
 import PanelCell from './internal/PanelCell.js';
-import {Slider, Divider, TextField} from 'material-ui';
+import {Slider, Divider, TextField, DatePicker} from 'material-ui';
 
 class Page extends Component {
     render() {
@@ -11,7 +11,7 @@ class Page extends Component {
                 <Panel header="Overview">
                     <PanelRow>
                         <PanelCell colClass="s4">
-                           
+
                             <TextField
                                 floatingLabelText="Number of unique visitors"
                                 value="100"
@@ -36,7 +36,7 @@ class Page extends Component {
                                     <Slider value={0}> </Slider>
                                 </PanelCell>
                             </PanelRow>
-                             <TextField
+                            <TextField
                                 floatingLabelText="Pageviews per visit (avg)"
                                 value="4"
                                 />
@@ -44,13 +44,13 @@ class Page extends Component {
                                 floatingLabelText="Time spent per page (avg)"
                                 value="00:30"
                                 />
-                                
+
                         </PanelCell>
-                        
-                        
+
+
                         <PanelCell colClass="s4">
-                        <div>Traffic distribution</div>
-                        <PanelRow style={{ padding: '15px 0' }}>
+                            <div>Traffic distribution</div>
+                            <PanelRow style={{ padding: '15px 0' }}>
                                 <PanelCell colClass="s3">
                                     micro
                                 </PanelCell>
@@ -67,9 +67,10 @@ class Page extends Component {
                                 </PanelCell>
                             </PanelRow>
                         </PanelCell>
+                        
                         <PanelCell colClass="s4">
-                        <div>Location</div>
-                        <PanelRow style={{ padding: '15px 0' }}>
+                            <div>Location</div>
+                            <PanelRow style={{ padding: '15px 0' }}>
                                 <PanelCell colClass="s3">
                                     Europe, Middle East, Africa
                                 </PanelCell>
@@ -95,6 +96,184 @@ class Page extends Component {
                             </PanelRow>
                         </PanelCell>
                     </PanelRow>
+
+                    <PanelRow>
+                        <PanelCell colClass="s4">
+                            <div>Start Date</div>
+                            <DatePicker container="inline" />
+                            <div>End Date</div>
+                            <DatePicker container="inline" />
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s12">
+                                    Annual increase in traffic
+                                </PanelCell>
+                                <PanelCell colClass="s12">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                        </PanelCell>
+                        <PanelCell colClass="s4">
+                         <div>Daily distribution</div>
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s3">
+                                    Monday
+                                </PanelCell>
+                                <PanelCell colClass="s9">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s3">
+                                    Tuesday
+                                </PanelCell>
+                                <PanelCell colClass="s9">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s3">
+                                    Wednesday
+                                </PanelCell>
+                                <PanelCell colClass="s9">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s3">
+                                    Thursday
+                                </PanelCell>
+                                <PanelCell colClass="s9">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s3">
+                                    Friday
+                                </PanelCell>
+                                <PanelCell colClass="s9">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s3">
+                                    Saturday
+                                </PanelCell>
+                                <PanelCell colClass="s9">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s3">
+                                    Sunday
+                                </PanelCell>
+                                <PanelCell colClass="s9">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                        </PanelCell>
+                        
+                        <PanelCell colClass="s4">
+                        <div>Daily distribution</div>
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s3">
+                                    January
+                                </PanelCell>
+                                <PanelCell colClass="s9">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s3">
+                                    February
+                                </PanelCell>
+                                <PanelCell colClass="s9">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s3">
+                                    March
+                                </PanelCell>
+                                <PanelCell colClass="s9">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s3">
+                                    April
+                                </PanelCell>
+                                <PanelCell colClass="s9">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s3">
+                                    May
+                                </PanelCell>
+                                <PanelCell colClass="s9">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s3">
+                                    June
+                                </PanelCell>
+                                <PanelCell colClass="s9">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s3">
+                                    July
+                                </PanelCell>
+                                <PanelCell colClass="s9">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s3">
+                                    August
+                                </PanelCell>
+                                <PanelCell colClass="s9">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s3">
+                                    September
+                                </PanelCell>
+                                <PanelCell colClass="s9">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s3">
+                                    October
+                                </PanelCell>
+                                <PanelCell colClass="s9">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s3">
+                                    November
+                                </PanelCell>
+                                <PanelCell colClass="s9">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                            <PanelRow style={{ padding: '15px 0' }}>
+                                <PanelCell colClass="s3">
+                                    December
+                                </PanelCell>
+                                <PanelCell colClass="s9">
+                                    <Slider value={0}> </Slider>
+                                </PanelCell>
+                            </PanelRow>
+                        </PanelCell>
+                        
+                    </PanelRow>
+
                 </Panel>
             </div>
         );
