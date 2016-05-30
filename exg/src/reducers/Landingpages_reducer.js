d075ed3782922906eacac4859c586ec0812caeba
dk\mrn <max.reimer.nielsen@gmail.com>
2016-05-30 08:55:44 +0200

    Reduxify LandingPages: Reducer prepared

--- exg/src/reducers/Landingpages_reducer.js
+++ exg/src/reducers/Landingpages_reducer.js
@@ -1,15 +1,15 @@
 import { LANDINGPAGES_GET, LANDINGPAGES_UPDATE } from '../constants/ActionTypes';
 import { combineReducers } from 'redux';
 
-const landingPages = (state = [], action) => {
+const landingPages = (state = {}, action) => {
     switch (action.type) {
         case LANDINGPAGES_GET:
-            return [...state];
+            return state;
             
         case LANDINGPAGES_UPDATE:
-            // TODO: Add update reducer logic
+            // TODO: Add update reducer logic.
             
-            return [...state];
+            return state;
     }
     
     return state;
--- exg/src/reducers/reducers.js
+++ exg/src/reducers/reducers.js
@@ -1,5 +1,10 @@
 import channels from './Channels_reducer';
+import landingPages from './LandingPages_reducer';
 import outcomes from './Outcomes_reducer';
 import { combineReducers } from 'redux';
 
-export default combineReducers({ channels, outcomes }); 
+export default combineReducers({ 
+    channels,
+    landingPages, 
+    outcomes 
+}); 
