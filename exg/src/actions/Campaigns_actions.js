d4baa07340d3c352a209d9a70e644124a2e7691f
dk\mrn <max.reimer.nielsen@gmail.com>
2016-05-31 11:24:54 +0200

    ReduxifyCampaigns: Added dummy actions

---
+++ exg/src/actions/Campaigns_actions.js
@@ -0,0 +1,13 @@
+import { CAMPAIGNS_GET, CAMPAIGNS_TRAFFIC_UPDATE, CAMPAIGNS_UPDATE } from '../constants/ActionTypes';
+
+export function getData() {
+    console.log('getData()');
+}
+
+export function updateCampaigns() {
+    console.log('updateCampaigns()');
+}
+
+export function updateCampaignTraffic() {
+    console.log('updateCampaignTraffic()');
+}
