f0bf802b7e6f75faaa9a0d7d2d7486653a987189
dk\msc <msc@sitecore.net>
2016-05-31 10:32:06 +0200

    Re-added my refurls reducers and actiontypes
    Had some merge issues..

--- exg/src/reducers/reducers.js
+++ exg/src/reducers/reducers.js
@@ -1,3 +1,4 @@
+import refUrls from './RefUrls_reducer';
 import channels from './Channels_reducer';
 import landingPages from './LandingPages_reducer';
 import outcomes from './Outcomes_reducer';
@@ -5,6 +6,7 @@ import dashboard from './Dashboard_reducer';
 import { combineReducers } from 'redux';
 
 export default combineReducers({ 
+    refUrls,
     channels,
     dashboard,
     landingPages, 
